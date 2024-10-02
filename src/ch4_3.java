df62811e522a4b950d798f05ffdc2767852dd428
박용진 <dydwls2227@naver.com>
2024-10-02 11:29:39 +0900

    실습문제 4-3

--- src/ch4_3.java
+++ src/ch4_3.java
@@ -1,4 +1,5 @@
-public class ch4_2 {
+// 4장 실습문제 3번
+public class ch4_3 {
     public static void main(String[] args) {
         for (int i = 1; i<=4; i++){
             for (int j = 1; j<=4-i; j++){
